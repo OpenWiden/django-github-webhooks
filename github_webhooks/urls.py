44c99636487b723bb78b35fe89c1ce0ed119feb6
Alexandr <stefanitsky.mozdor@gmail.com>
2020-06-14 11:36:54 +0300

    :ok_hand: webhook url path changed to empty string

--- github_webhooks/urls.py
+++ github_webhooks/urls.py
@@ -4,5 +4,5 @@ from .views import github_webhook_view
 app_name = "github_webhook"
 
 urlpatterns = [
-    path("receive/", github_webhook_view, name="receive"),
+    path("", github_webhook_view, name="receive"),
 ]
