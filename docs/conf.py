3438f649042bd43ce1f4ba3ab147453cfb6aae48
Alexandr <stefanitsky.mozdor@gmail.com>
2020-06-14 04:08:12 +0300

    :wrench: fixes readthedocs/readthedocs.org#2569

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The master toctree document.
+# Note: https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
